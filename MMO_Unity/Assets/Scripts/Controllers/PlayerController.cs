b55fe06db6400fdc03f2e1d59fd77a8b8669cd5f
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-05-19 20:27:03 +0900

    SetCursor

--- MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -9,6 +9,18 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerStat _stat;
     private Vector3 _destPos;
+
+    private Texture2D _attackIcon;
+    private Texture2D _handIcon;
+
+    enum CursorType
+    {
+        None,
+        Attack,
+        Hand,
+    }
+
+    private CursorType _cursorType = CursorType.None;
     
     public enum PlayerState
     {
@@ -22,6 +34,9 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
+        _attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
+
         _stat = GetComponent<PlayerStat>();
         
         Managers.Input.MouseAction -= OnMouseClicked;
@@ -70,6 +85,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        UpdateMouseCursor();
         switch (_state)
         {
             case PlayerState.Die:
@@ -81,6 +97,32 @@ public class PlayerController : MonoBehaviour
             case PlayerState.Idle:
                 UpdateIdle();
                 break;
+        }
+    }
+
+    void UpdateMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, _mask))
+        {
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                if (_cursorType != CursorType.Attack)
+                {
+                    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5, 0), CursorMode.Auto);
+                    _cursorType = CursorType.Attack;
+                }
+            }
+            else
+            {
+                if (_cursorType != CursorType.Hand)
+                {
+                    Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3, 0), CursorMode.Auto);
+                    _cursorType = CursorType.Hand;
+                }
+            }
         }
     }
 
